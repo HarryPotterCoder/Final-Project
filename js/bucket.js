2221fea08d888f529f55490de8ecdec3d7ec0e0a
HarryPotterCoder <32655787+HarryPotterCoder@users.noreply.github.com>
2018-05-01 19:25:29 -0400

    FInal
    5/1 Edits

--- js/bucket.js
+++ js/bucket.js
@@ -1,9 +1,11 @@
-$(document).ready(function() {
- // jQuery UI Widgets
+$(document).ready(function(){
+ $('.header').height($(window).height());
 
-  $(".navbar a").click(function(){
-  $("body,html").animate({
-   scrollTop:$("#" + $(this).data('value')).offset().top
-  },1000)
+ $(".navbar a").click(function(){
+ 	$("body,html").animate({
+ 		scrollTop:$("#" + $(this).data('value')).offset().top
+ 	},1000)
   
- });
+ })
+
+})
